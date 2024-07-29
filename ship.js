2340d4efe642f2c871f3d986e1cbec271bfd71c2
sinskiy <sinskiy@proton.me>
2024-07-29 10:38:51 +0300

    feat: implement Ship class

--- ship.js
+++ ship.js
@@ -1,1 +1,14 @@
-export default class Ship {}
+export default class Ship {
+  constructor(length) {
+    // if (length < 1 || length > 4 ) throw new Error("Size must be between 1 and 4")
+
+    this.length = length;
+    this.hits = 0;
+  }
+  hit() {
+    this.hits++;
+  }
+  isSunk() {
+    return this.hits >= this.length;
+  }
+}
--- ship.test.js
+++ ship.test.js
@@ -1,6 +1,26 @@
 import Ship from "./ship";
 
+const ship = new Ship(3);
 describe("Ship creates correctly", () => {
-  const ship = new Ship();
+  test("Ship has correct length", () => {
+    expect(ship.length).toBe(3);
+  });
+  test("Ship has correct hits", () => {
+    expect(ship.hits).toBe(0);
+  });
 });
-describe("Ship behaves correctly", () => {});
+
+describe("Ship behaves correctly", () => {
+  test("Ship is not sunk when hits is 0 and length is 3", () => {
+    expect(ship.isSunk()).toBe(false);
+  });
+  test("Ship is not sunk when hits is 2 and length is 3", () => {
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBe(false);
+  });
+  test("Ship is sunk when hits is 3 and length is 3", () => {
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+  });
+});
