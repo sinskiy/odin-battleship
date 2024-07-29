20fa28f5400eda0d68928039ccf6cd4180db0b29
sinskiy <sinskiy@proton.me>
2024-07-29 20:00:51 +0300

    feat: add method to determine if all ships sunk

--- gameboard.js
+++ gameboard.js
@@ -8,27 +8,36 @@ export default class Gameboard {
     this.shots = Array.from({ length: BOARD_SIZE }, () =>
       Array(BOARD_SIZE).fill(false)
     );
+    this.placedShips = 0;
+    this.sankShips = 0;
   }
   place(x, y, length, horizontal = true) {
     const ship = new Ship(length);
     if (horizontal) {
       const xEnd = x + length;
       for (let i = x; i < xEnd; i++) {
-        this.ships[y][i] = ship;
+        this.ships[i][y] = ship;
       }
     } else {
       const yEnd = y + length;
       for (let i = y; i < yEnd; i++) {
-        this.ships[i][x] = ship;
+        this.ships[x][i] = ship;
       }
     }
+
+    this.placedShips++;
   }
   receiveAttack(x, y) {
-    this.shots[y][x] = true;
+    this.shots[x][y] = true;
 
-    const ship = this.ships[y][x];
-    if (ship) {
-      ship.hits++;
-    }
+    const ship = this.ships[x][y];
+    if (!ship) return;
+
+    ship.hits++;
+
+    if (ship.isSunk()) this.sankShips++;
+  }
+  allSunk() {
+    return this.placedShips === this.sankShips;
   }
 }
--- gameboard.test.js
+++ gameboard.test.js
@@ -5,6 +5,8 @@ const gameboard = new Gameboard();
 describe("Gameboard creates correctly", () => {
   test("Gameboard has no ships", () => {
     expect(gameboard.ships.flat().filter((x) => x)).toHaveLength(0);
+    expect(gameboard.placedShips).toBe(0);
+    expect(gameboard.sankShips).toBe(0);
   });
 });
 describe("Gameboard behaves correctly", () => {
@@ -12,10 +14,14 @@ describe("Gameboard behaves correctly", () => {
     gameboard.place(0, 0, 4);
     const ship = new Ship(4);
     for (let i = 0; i < 4; i++) {
-      expect(gameboard.ships[0][i]).toEqual(ship);
+      expect(gameboard.ships[i][0]).toEqual(ship);
     }
-    expect(gameboard.ships[0][5]).toBeUndefined();
-    expect(gameboard.ships[1][0]).toBeUndefined();
+
+    expect(gameboard.placedShips).toBe(1);
+    expect(gameboard.sankShips).toBe(0);
+
+    expect(gameboard.ships[5][0]).toBeUndefined();
+    expect(gameboard.ships[0][1]).toBeUndefined();
   });
   test("Gameboard has a ship at the 4,3 and a ship at the 0,0", () => {
     const x = 4;
@@ -26,16 +32,51 @@ describe("Gameboard behaves correctly", () => {
 
     const ship = new Ship(length);
     for (let i = y; i < length; i++) {
-      expect(gameboard.ships[i][x]).toEqual(ship);
+      expect(gameboard.ships[x][i]).toEqual(ship);
     }
+
+    expect(gameboard.placedShips).toBe(2);
+    expect(gameboard.sankShips).toBe(0);
 
     expect(gameboard.ships.flat().filter((x) => x)).toHaveLength(6);
   });
   test("Gameboard receives hits correctly", () => {
     gameboard.receiveAttack(4, 3);
-    expect(gameboard.ships[3][4].hits).toBe(1);
+    expect(gameboard.ships[4][3].hits).toBe(1);
     expect(gameboard.ships[4][4].hits).toBe(1);
     expect(gameboard.ships[0][0].hits).toBe(0);
-    expect(gameboard.shots[3][4]).toBe(true);
+    expect(gameboard.shots[4][3]).toBe(true);
+
+    expect(gameboard.placedShips).toBe(2);
+    expect(gameboard.sankShips).toBe(0);
+
+    expect(gameboard.allSunk()).toBe(false);
+  });
+  test("Gameboard sanks correctly", () => {
+    gameboard.receiveAttack(4, 4);
+    expect(gameboard.ships[4][3].hits).toBe(2);
+    expect(gameboard.ships[4][4].hits).toBe(2);
+    expect(gameboard.ships[4][3].isSunk()).toBe(true);
+    expect(gameboard.ships[0][0].hits).toBe(0);
+    expect(gameboard.shots[4][4]).toBe(true);
+
+    expect(gameboard.placedShips).toBe(2);
+    expect(gameboard.sankShips).toBe(1);
+
+    expect(gameboard.allSunk()).toBe(false);
+  });
+  test("Gameboard determines if all ships sunk correctly", () => {
+    gameboard.receiveAttack(0, 0);
+    expect(gameboard.ships[0][0].hits).toBe(1);
+    gameboard.receiveAttack(1, 0);
+    expect(gameboard.ships[0][0].hits).toBe(2);
+    gameboard.receiveAttack(2, 0);
+    gameboard.receiveAttack(3, 0);
+    expect(gameboard.ships[0][0].hits).toBe(4);
+    expect(gameboard.ships[0][0].isSunk()).toBe(true);
+
+    expect(gameboard.sankShips).toBe(2);
+
+    expect(gameboard.allSunk()).toBe(true);
   });
 });
