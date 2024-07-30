a528c876ff1c788e97b091927e10592c7c942947
sinskiy <sinskiy@proton.me>
2024-07-30 09:25:10 +0300

    feat: display board

--- src/game.js
+++ src/game.js
@@ -1,10 +1,15 @@
 import Player from "./player";
 
-const player1 = new Player("player");
-const player2 = new Player("computer");
-player1.board.place(0, 0, 5);
-player2.board.place(0, 0, 5);
-player1.board.place(1, 0, 4);
-player2.board.place(1, 0, 4);
-player1.board.place(2, 0, 3);
-player2.board.place(2, 0, 3);
+export function createGame() {
+  const player1 = new Player("player");
+  const player2 = new Player("computer");
+  player1.board.place(0, 0, 5);
+  player2.board.place(0, 0, 5);
+  player1.board.place(0, 1, 4);
+  player2.board.place(0, 1, 4);
+  player1.board.place(0, 2, 3);
+  player2.board.place(0, 2, 3);
+  player1.board.place(0, 3, 2);
+  player2.board.place(0, 3, 2);
+  return { player1, player2 };
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-import { BOARD_SIZE } from "./gameboard";
+import { createGame } from "./game";
+import Ship from "./ship";
+import "./style.css";
 
-console.log(BOARD_SIZE);
+createGameDOM();
+
+function createGameDOM() {
+  const { player1, player2 } = createGame();
+  const board1 = boardToDOM(player1);
+  const board2 = boardToDOM(player2);
+  document.body.append(board1, board2);
+}
+
+function boardToDOM(player) {
+  const DOMBoard = document.createElement("div");
+  DOMBoard.classList.add("board", player.type);
+  // const board2 = document.createElement("div");
+  for (const column of player.board.ships) {
+    const boardColumn = document.createElement("div");
+    boardColumn.classList.add("column");
+    for (const row of column) {
+      const boardRow = document.createElement("div");
+      boardRow.classList.add("row", row instanceof Ship && "ship");
+
+      boardColumn.appendChild(boardRow);
+    }
+
+    DOMBoard.appendChild(boardColumn);
+  }
+
+  return DOMBoard;
+}
